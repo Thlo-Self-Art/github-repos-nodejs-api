7a9234bdcef0d13f84fb590f215499b37fa97d0d
Thomas Luo <thlo7777@qq.com>
2023-11-02 11:43:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', async (req, res) => {
   const username = req.query.username || 'thlo7777';
   try {
     const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
+      `https://api.github.com/users/thlo7777/repos`
     );
     const repos = result.data
       .map((repo) => ({
